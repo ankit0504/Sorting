5c02aec6caffc37c72c80f23afa10ec5d9b09b28
Ankit Gupta <ankit0504@gmail.com>
2018-07-26 17:41:41 -0400

    counting and insertion sort

--- test_counting.py
+++ test_counting.py
@@ -3,47 +3,45 @@ import random
 def counting_sort(given_list):
     if len(given_list) < 2:
         return given_list
-    num_instances = [0 for i in range(256)]
-    returnable = [0 for i in range(256)]
+    num_instances = [0 for i in range(max(given_list)+1)]
+    returnable = [0 for i in range(len(given_list))]
     for num in given_list:
         num_instances[num] += 1
-    for i in range(256):
+    for i in range(1, max(given_list)+1):
         num_instances[i] += num_instances[i-1]
     for i in reversed(given_list):
-        returnable[num_instances[i]] = i
+        returnable[num_instances[i]-1] = i
         num_instances[i] -= 1
     return returnable
 
 
 def test_already_sorted():
-    already_sorted = [0,1,2,3,4,5,6,7,8,255]
+    already_sorted = [0,1,2,3,4,5,6,7,8,9]
     assert counting_sort(already_sorted) == sorted(already_sorted)
 
 
-# def test_decrementing():
-#     decrementing = [255,8,7,6,5,4,3,2,1,0]
-#     assert counting_sort(decrementing) == sorted(decrementing)
+def test_decrementing():
+    decrementing = [255,8,7,6,5,4,3,2,1,0]
+    assert counting_sort(decrementing) == sorted(decrementing)
 
 
-# def test_random_numbers():
-#     rand_nums = random.sample(range(100), 10)
-#     assert counting_sort(rand_nums) == sorted(rand_nums)
+def test_random_numbers():
+    rand_nums = random.sample(range(100), 10)
+    assert counting_sort(rand_nums) == sorted(rand_nums)
 
 
-# def test_empty_list():
-#     assert counting_sort([]) == []
+def test_empty_list():
+    assert counting_sort([]) == []
 
 
-# def test_one_element_list():
-#     rand_num = random.randint(0,100)
-#     assert counting_sort([rand_num]) == [rand_num]
+def test_one_element_list():
+    rand_num = random.randint(0,100)
+    assert counting_sort([rand_num]) == [rand_num]
 
 
-# def test_random_negatives():
-#     rand_nums = random.sample(range(-100,-1), 5)
-#     assert counting_sort(rand_nums) == sorted(rand_nums)
+def test_has_duplicates():
+    rand_nums = [random.randint(1,5) for x in range(1, 10)]
+    assert counting_sort(rand_nums) == sorted(rand_nums)
 
-
-# def test_has_duplicates():
-#     rand_nums = [random.randint(1,5) for x in range(1, 10)]
-#     assert counting_sort(rand_nums) == sorted(rand_nums)
+# No test for random negatives because this counting sort cannot handle negatives.
+# You can implement an offset and make it work
---
+++ test_insertion.py
@@ -0,0 +1,44 @@
+import random
+
+def insertion_sort(given_list):
+    for i in range (len(given_list)):
+        prev = i - 1
+        while prev >= 0 and given_list[i] < given_list[prev]:
+            given_list[prev + 1] = given_list[prev]
+            prev -= 1
+        given_list[prev + 1] = given_list[i]
+    return given_list
+
+
+def test_already_sorted():
+    already_sorted = [0,1,2,3,4,5,6,7,8,9]
+    assert insertion_sort(already_sorted) == sorted(already_sorted)
+
+
+def test_decrementing():
+    decrementing = [9,8,7,6,5,4,3,2,1,0]
+    assert insertion_sort(decrementing) == sorted(decrementing)
+
+
+def test_random_numbers():
+    rand_nums = random.sample(range(100), 10)
+    assert insertion_sort(rand_nums) == sorted(rand_nums)
+
+
+def test_empty_list():
+    assert insertion_sort([]) == []
+
+
+def test_one_element_list():
+    rand_num = random.randint(0,100)
+    assert insertion_sort([rand_num]) == [rand_num]
+
+
+def test_random_negatives():
+    rand_nums = random.sample(range(-100,-1), 5)
+    assert insertion_sort(rand_nums) == sorted(rand_nums)
+
+
+def test_has_duplicates():
+    rand_nums = [random.randint(1,5) for x in range(1, 10)]
+    assert insertion_sort(rand_nums) == sorted(rand_nums)
